e6a8cdf96d6c43a1503b961178e658cd6a7b8634
Michael Price <mikkyx@gmail.com>
2018-02-15 23:20:01 +0000

    update game name in meta title

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Name That Tune [ Larahack 2018 ]</title>
+    <title>SongXpert</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.css" rel="stylesheet" type="text/css">
     <script src="/js/fontawesome.min.js"></script>
     <script src="/js/fa-brands.min.js"></script>
